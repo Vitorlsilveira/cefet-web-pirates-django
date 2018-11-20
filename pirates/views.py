ffb8411f9dbb566a1d5f45e54a2a34da7c5f9c0c
Vitor <vitorlsilveira10@gmail.com>
2018-11-19 23:26:14 -0200

    Pratica piratas django

--- pirates/views.py
+++ pirates/views.py
@@ -1,2 +1,37 @@
 from django.shortcuts import render
 from django.views import View
+from django.db.models import F,ExpressionWrapper,DecimalField, Sum
+from pirates.models import Tesouro
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+class ListaTesourosView(View):
+    def get(self, request):
+        return render(request, "lista_tesouros.html",{'lista_tesouros': Tesouro.objects.annotate(valor_total=ExpressionWrapper(F('preco')*F('quantidade'),output_field=DecimalField(max_digits=10,decimal_places=2,blank=True))) , "total_geral" : Tesouro.objects.aggregate(total_geral = Sum(ExpressionWrapper(F('preco')*F('quantidade'),output_field=DecimalField(max_digits=10,decimal_places=2,blank=True))))['total_geral']})
+
+class TesouroForm(forms.ModelForm):
+    class Meta:
+        model = Tesouro
+        fields = ['nome', 'quantidade', 'preco', 'img_tesouro' ]
+        labels = {"nome": "Nome", "quantidade" : "Quantidade", "preco" : "Preco", "img_tesouro" : "Imagem"}
+
+class SalvarTesouro(View):
+    def get(self,request, id=None): #Requisitou a exibição do formulário
+        tesouro = Tesouro.objects.get(id=id) if id != None else None
+        return render(request,"salvar_tesouro.html",{"tesouro":TesouroForm(instance=tesouro)})
+
+    def post(self,request,id=None):#via post, salva o tesouro
+        tesouro = Tesouro.objects.get(id=id) if id != None else None
+        form = TesouroForm(request.POST,request.FILES, instance=tesouro)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('lista_tesouros') )
+        else:
+            return render(request,"salvar_tesouro.html",{"tesouro":form})
+
+class RemoverTesouro(View):
+    def get(self,request, id):
+        Tesouro.objects.filter(id = id).delete()
+        return HttpResponseRedirect(reverse('lista_tesouros'))
--- web_pirates/urls.py
+++ web_pirates/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from pirates.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('', ListaTesourosView.as_view(), name='lista_tesouros'),
+    path('novo-tesouro/', SalvarTesouro.as_view(), name='salvar_tesouro'),
+    path('novo-tesouro/<int:id>', SalvarTesouro.as_view(), name='salvar_tesouro'),
+    path('remover-tesouro/<int:id>/', RemoverTesouro.as_view(),name='remover_tesouro'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
